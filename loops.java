e4ef4d95a28acf6d97e98bf9cdbe49ddbd9b354f
moolyaed <120448811+moolyaed@users.noreply.github.com>
2023-01-18 15:24:18 +0530

    first commit

---
+++ loops.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=" Hanish reddy ";
+        System.out.println(str.length());
+        System.out.println(str.trim());
+        System.out.println(str.indexOf("reddy"));
+        int a=Integer.parseInt(sc.nextLine());
+        System.out.println(a);
+
+        }}
